87f10255b05555685263f4b37121e33ec9053e06
mukondono <nathanielreline@gmail.com>
2015-10-18 19:09:15 -0700

    Added Content Provider

--- Earthquake/app/src/main/java/com/natrelin/earthquake/Earthquake.java
+++ Earthquake/app/src/main/java/com/natrelin/earthquake/Earthquake.java
@@ -2,6 +2,8 @@ package com.natrelin.earthquake;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,6 +13,7 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class Earthquake extends Activity {
 
@@ -26,6 +29,11 @@ public class Earthquake extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
         updateFromPreferences();
+
+        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        SearchView searchView = (SearchView)findViewById(R.id.searchView);
+        searchView.setSearchableInfo(searchableInfo);
     }
 
     @Override
--- Earthquake/app/src/main/java/com/natrelin/earthquake/EarthquakeListFragment.java
+++ Earthquake/app/src/main/java/com/natrelin/earthquake/EarthquakeListFragment.java
@@ -1,6 +1,10 @@
 package com.natrelin.earthquake;
 
 import android.app.ListFragment;
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -36,18 +41,24 @@ import javax.xml.parsers.ParserConfigurationException;
  * A placeholder fragment containing a simple view.
  */
 public class EarthquakeListFragment extends ListFragment {
-    ArrayAdapter<Quake> aa;
-    ArrayList<Quake> earthquakes = new ArrayList<Quake>();
+    //ArrayAdapter<Quake> aa;
+    ArrayList<Quake> earthquakes = new ArrayList<>();
 
     private static final String TAG = "EARTHQUAKE";
     private Handler handler = new Handler();
 
+    SimpleCursorAdapter adapter;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        int layoutID = android.R.layout.simple_list_item_1;
+        /*int layoutID = android.R.layout.simple_list_item_1;
         aa = new ArrayAdapter<>(getActivity(), layoutID, earthquakes);
-        setListAdapter(aa);
+        setListAdapter(aa);*/
+
+        adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1, null,
+                new String[] {EarthquakeProvider.KEY_SUMMARY }, new int[] {android.R.id.text1 }, 0);
+        setListAdapter(adapter);
 
         Thread t = new Thread(new Runnable() {
             @Override
@@ -156,13 +167,33 @@ public class EarthquakeListFragment extends ListFragment {
     }
 
     private void addNewQuake(Quake _quake) {
-        Earthquake earthquakeActivity = (Earthquake) getActivity();
+        ContentResolver cr = getActivity().getContentResolver();
+
+        String w = EarthquakeProvider.KEY_DATE + "=" + _quake.getDate().getTime();
+
+        Cursor query = cr.query(EarthquakeProvider.CONTENT_URI, null, w, null, null);
+        if(query.getCount() == 0) {
+            ContentValues values = new ContentValues();
+            values.put(EarthquakeProvider.KEY_DATE, _quake.getDate().getTime());
+            values.put(EarthquakeProvider.KEY_DETAILS, _quake.getDetails());
+            values.put(EarthquakeProvider.KEY_SUMMARY, _quake.toString());
+            double lat = _quake.getLocation().getLatitude();
+            double lng = _quake.getLocation().getLatitude();
+            values.put(EarthquakeProvider.KEY_LOCATION_LAT, lat);
+            values.put(EarthquakeProvider.KEY_LOCATION_LNG, lng);
+            values.put(EarthquakeProvider.KEY_LINK, _quake.getLink());
+            values.put(EarthquakeProvider.KEY_MAGNITUDE, _quake.getMagnitude());
+            cr.insert(EarthquakeProvider.CONTENT_URI, values);
+        }
+        query.close();
+
+        /*Earthquake earthquakeActivity = (Earthquake) getActivity();
         if(_quake.getMagnitude() > earthquakeActivity.minimumMagnitude) {
             // add the new quake to our list of earthquakes
             earthquakes.add(_quake);
         }
 
         // notify the array adapter of a change
-        aa.notifyDataSetChanged();
+        aa.notifyDataSetChanged();*/
     }
 }
